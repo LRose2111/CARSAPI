2b1d7bc05ea3a32df4c7e08cdb69ba95c8477443
Lakota Rose <itsrosebro@gmail.com>
2022-03-30 15:39:16 -0500

    Updated Cars/views.py file.

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import Carserializer
+from .models import Car
+from cars import serializers
 
 @api_view('GET')
 def cars_list(request):
+    cars = Car.objects.all()
 
 
+    serializer = Carserializer(cars, many=True)
+
+    
 
     return Response('ok')
-    
+
